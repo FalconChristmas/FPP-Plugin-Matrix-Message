990135fe644b0395412cb4d1fa059a478bf01ada
Poporacer <46263877+Poporacer@users.noreply.github.com>
2021-08-02 23:04:52 -0600

    Update to use 5.0 functionality

--- excluded_plugins.inc.php
+++ excluded_plugins.inc.php
@@ -2,8 +2,6 @@
 //put the plugins that should NOT be included in the matrix output
 //this would probably be better if it was changed to be an included array and add items as installed?
 
-$EXCLUDE_PLUGIN_ARRAY = array("FPP-Plugin-Cron-Editor","FPP-Plugin-BetaBriteMessage","FPP-Plugin-BetaBrite","FPP-Plugin-Matrix-Message","fpp-matrixtools","fpp-edmrds","FPP-Plugin-MessageQueue","FPP-Plugin-ProjectorControl","Nagios","LiveMatrix", "fpp-BigButtons");
-
-
+$EXCLUDE_PLUGIN_ARRAY = array("FPP-Plugin-Cron-Editor","FPP-Plugin-BetaBriteMessage","FPP-Plugin-BetaBrite","FPP-Plugin-Matrix-Message","fpp-matrixtools","fpp-edmrds","FPP-Plugin-MessageQueue","FPP-Plugin-ProjectorControl","Nagios","LiveMatrix", "fpp-BigButtons","fpp-vastfmt", "Si4713_FM_RDS", "fpp-after-hours", "fpp-brightness", "fpp-arcade", "fpp-HomeAssistant", "remote-falcon", "show-on-demand", "fpp-smartthings-plugin" );
 
 ?>
